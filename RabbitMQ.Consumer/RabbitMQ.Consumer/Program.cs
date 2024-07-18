fd5c486a4a2482d64884e09a77a3f4bf1917912d
nuhaydogdu <nuhaydd25@gmail.com>
2024-07-18 16:23:17 +0300

    Details of Creating Simple Publisher and Consumer Applications

---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://bcroqslz:aLcvABvoIwvfRYRxNkKz5-d3O2jvnDHH@shark.rmq.cloudamqp.com/bcroqslz");
+
+//Bağlantıyı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue Oluşturma 
+//Consumer'da da kuyruk publisher'daki ile birebir aynı yapılandırmada tanımlanmalıdır!
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue'ye Mesaj Okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajın işlendiği yerdir!
+    //e.Body: Kuyruktaki mesajın verisini bütünsel olarak getiriecektir.
+    //e.Body.Span veya e.Body.ToArray(): Kuyruktaki mesajın byte verisini getirecektir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.ReadLine();
---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://bcroqslz:aLcvABvoIwvfRYRxNkKz5-d3O2jvnDHH@shark.rmq.cloudamqp.com/bcroqslz");
+
+//Bağlantıyı Aktifleştirme ve Kanal Açma
+using IConnection connection =factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue Oluşturma 
+channel.QueueDeclare(queue: "example-queue",exclusive: false);
+
+//Queue'ye Mesaj Gönderme
+//RabbitMQ kuyruğa atacağı mesajları byte türünde kabul etmektedir. Haliyle mesajları bizim byte dönüştürmemiz gerekecektir.
+//default exchange, direkt excahange olacak. Böyle olunca da hangi kuyruğa mesaj göndereceksek o kuyruğun ismini routingKey'de bildirmiş olduk.
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+Console.ReadLine(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
